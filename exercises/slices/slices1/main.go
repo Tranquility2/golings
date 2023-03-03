3ec4ccdd2ef756ede58063b165e2202a24916344
Roy Moore <roy@moore.co.il>
2023-03-03 13:26:39 +0200

    fixed slices1

--- exercises/slices/slices1/main.go
+++ exercises/slices/slices1/main.go
@@ -1,13 +1,9 @@
-// slices1
-// Make me compile!
-
-// I AM NOT DONE
 package main
 
 import "fmt"
 
 func main() {
-	a := make(, 3, 10) // play with length and capacity
+	a := make([]string, 5, 10) // play with length and capacity
 	fmt.Println("length of 'a':", len(a))
 	fmt.Println("capacity of 'a':", cap(a))
 }
