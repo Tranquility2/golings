3b2dc753ec1651655af8002c6949dcb5665cb751
Roy Moore <roy@moore.co.il>
2023-03-04 14:30:33 +0200

    fixed structs2

--- exercises/structs/structs2/main.go
+++ exercises/structs/structs2/main.go
@@ -1,19 +1,23 @@
-// structs2
-// Make me compile!
-//
-// I AM NOT DONE
 package main
 
 import "fmt"
 
 type Person struct {
-	// don't just create the phone field here. embed a new struct
 	name string
-	age int
+	age  int
+}
+
+type ContactDetails struct {
+	person Person
+	phone  string
 }
 
 func main() {
-	// contactDetails := ContactDetails{}
 	person := Person{name: "John", age: 32}
-	fmt.Printf("%s is %d years old and his phone is %s\n", person.name, person.age, person.phone)
+	contactDetails := ContactDetails{person: person, phone: "+01 333 666"}
+
+	fmt.Printf("%s is %d years old and his phone is %s\n",
+		contactDetails.person.name,
+		contactDetails.person.age,
+		contactDetails.phone)
 }
