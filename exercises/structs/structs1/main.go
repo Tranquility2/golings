7b7f6e496822c1635edbd6f116c6e6243d4b140d
Roy Moore <roy@moore.co.il>
2023-03-04 14:20:20 +0200

    fixed structs1

--- exercises/structs/structs1/main.go
+++ exercises/structs/structs1/main.go
@@ -1,15 +1,13 @@
-// structs1
-// Make me compile!
-//
-// I AM NOT DONE
 package main
 
 import "fmt"
 
 type Person struct {
-
+	name string
+	age int
 }
 
 func main() {
+	person := Person{name: "Dan", age: 21}
 	fmt.Printf("Person %s and age %d", person.name, person.age)
 }
